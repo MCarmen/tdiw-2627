0eac9b9314a82eb25746d2b544094cd6b83ed43f
MCarmen <detoro.mcarmen@gmail.com>
2026-09-08 16:15:28 +0200

    Generating dynamically the paragraph tag
    Remove commented PHP code and add HTML comment for future reference.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!-- TODO: Copy the index.html code in here -->
 <!-- TODO: uncomment: 
 <h1><?php echo "Hello World"; ?><h1>
-<?php echo "I love TDIW class"; ?>  
+<?php echo "<p>I love TDIW class</p>"; ?>  
 -->  
