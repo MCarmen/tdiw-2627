4b30ddbf2b96fae81c34dc214b267bb1631ab962
MCarmen <detoro.mcarmen@gmail.com>
2026-09-24 10:54:39 +0200

    First version

---
+++ js/funcions.js
@@ -0,0 +1,7 @@
+function confirmaRegistre(){
+    alert("registrant estudiant");
+    let formDiv = document.getElementById("formDiv")
+    formDiv.innerHTML = "<p class='important'>T'has registrat amb èxit!</p>";
+    console.log("registrant estudiant");
+    return false;
+}
